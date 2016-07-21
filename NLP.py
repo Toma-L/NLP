a358216a2cd5d5f2940ce9168e8378008aa9c581
Toma-L <anthony41213@hotmail.com>
2016-07-21 17:10:03 +0800

    2.1

--- NLP.py
+++ NLP.py
@@ -236,3 +236,106 @@ for word in tricky:
 
 #2.1====================
 
+import nltk
+nltk.corpus.gutenberg.fileids()
+emma = nltk.corpus.gutenberg.words('austen-emma.txt')
+len(emma)
+emma = nltk.Text(nltk.corpus.gutenberg.words('austen-emma.txt'))
+emma.concordance('surprize')
+
+#another way to do this
+
+from nltk.corpus import gutenberg
+gutenberg.fileids()
+emma = gutenberg.words('austen-emma.txt')
+
+for fileid in gutenberg.fileids():
+    num_chars = len(gutenberg.raw(fileid))
+    num_words = len(gutenberg.words(fileid))
+    num_sents = len(gutenberg.sents(fileid))
+    num_vocab = len(set([w.lower() for w in gutenberg.words(fileid)]))
+    print (int(num_chars/num_words), int(num_words/num_sents)) #avg word & sentence length and the diversity of words
+
+macbeth_sentences = gutenberg.sents('shakespeare-macbeth.txt')
+macbeth_sentences #load sentences of Macbeth
+macbeth_sentences[1037]
+longest_len = max([len(s) for s in macbeth_sentences])
+[s for s in macbeth_sentences if len(s) == longest_len] #find longest sentence
+
+from nltk.corpus import webtext
+for fileid in webtext.fileids():
+    print (fileid, webtext.raw(fileid)[:65], '...')
+
+from nltk.corpus import nps_chat
+chatroom = nps_chat.posts('10-19-20s_706posts.xml')
+chatroom[123]
+
+from nltk.corpus import brown
+brown.categories()
+brown.words(categories = 'news') 
+brown.words(fileids = ['cg22'])
+
+from nltk.corpus import brown
+news_text = brown.words(categories = 'news')
+fdist = nltk.FreqDist([w.lower() for w in news_text])
+modals = ['can', 'could', 'may', 'might', 'must', 'will']
+for m in modals:
+    print (m + ':', fdist[m],)
+
+cfd = nltk.ConditionalFreqDist(
+        (genre, word)
+        for genre in brown.categories()
+        for word in brown.words(categories = genre))
+
+genres = ['news', 'religion', 'hobbies', 'science_fiction', 'romance', 'humor']
+modals = ['can', 'could', 'may', 'might', 'must', 'will']
+cfd.tabulate(conditions = genres, samples = modals)
+
+from nltk.corpus import reuters
+reuters.fileids()
+reuters.categories()
+
+reuters.categories('training/9865')
+reuters.categories(['training/9865', 'training/9880'])
+reuters.fileids('barley')
+reuters.fileids(['barley', 'corn'])
+
+reuters.words('training/9865')[:14]
+reuters.words(['training/9865', 'training/9880'])
+reuters.words(categories = 'barley')
+reuters.words(categories = ['barley', 'corn'])
+
+from nltk.corpus import inaugural
+inaugural.fileids()
+[fileid[:4] for fileid in inaugural.fileids()]
+
+cfd = nltk.ConditionalFreqDist(
+        (target, fileid[:4])
+        for fileid in inaugural.fileids()
+        for w in inaugural.words(fileid)
+        for target in ['america', 'citizen']
+        if w.lower().startswith(target))
+
+cfd.plot()
+
+nltk.corpus.cess_esp.words()
+nltk.corpus.floresta.words() #Error
+nltk.corpus.indian.words('hindi.pos')
+nltk.corpus.udhr.fileids()
+nltk.corpus.udhr.words('Javanese-Latin1')[11:]
+
+from nltk.corpus import udhr
+languages = ['Chickasaw', 'English', 'German_Deutsch', 'Greenlandic_Inuktikut', 'Hungarian_Magyar', 'Ibibio_Efik']
+cfd = nltk.ConditionalFreqDist(
+        (lang, len(word))
+        for lang in languages
+        for word in udhr.words(lang + '-Latin1'))
+cfd.plot(cumulative = True)
+
+raw = gutenberg.raw("burgess-busterbrown.txt")
+raw[1:20]
+words = gutenberg.words("burgess-busterbrown.txt")
+words[1:20]
+sents = gutenberg.sents("burgess-busterbrown.txt")
+sents[1:20]
+
